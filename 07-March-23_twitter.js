fad2224a5cffa9ced2e6f18363841a63e2f105c9
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-09-07 13:50:22 +0530

    Update 07-March-23_twitter.js

--- 07-March-23_twitter.js
+++ 07-March-23_twitter.js
@@ -16,6 +16,7 @@ const playwright = require('playwright');
 // TWITTER_USERNAME=
 
 // TWITTER_PASSWORD=
+
 const browserType = 'chromium'; // chrome
 const username = process.env.TWITTER_USERNAME;
 const password = process.env.TWITTER_PASSWORD;
