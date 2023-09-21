b937058d9fd3d52eb903edcef0dce3d03da37134
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-09-21 09:30:31 +0530

    Update 07-March-23_twitter.js

--- 07-March-23_twitter.js
+++ 07-March-23_twitter.js
@@ -20,7 +20,6 @@ async function main() {
   const browser = await playwright[browserType].launch({ headless: false });
   const page = await browser.newPage();
   await page.goto('http://twitter.com/login');
-  
   await page.waitForTimeout(1000);
   
   const inputUsername = await page.$('[name="session[username_or_email]"]');
