a2ed90544b50fd8c7ada4d5bf941fa9413a7da80
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-11-17 08:48:07 +0530

    Update 07-March-23_twitter.py

--- 07-March-23_twitter.py
+++ 07-March-23_twitter.py
@@ -71,6 +71,7 @@ class TwitterRecommendationFollower:
             # using an action chain to write the username and password
             # into the two input fields on the login screen
             print("Sending username and password")
+            
             action_chain = ActionChains(self.driver)
             action_chain.move_to_element(username_input).click()
             action_chain.send_keys(username)
