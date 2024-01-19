152130c50ebc381f855fb240617e515b268534ce
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-01-19 09:32:17 +0530

    Update 07-March-23_twitter.js

--- 07-March-23_twitter.js
+++ 07-March-23_twitter.js
@@ -30,6 +30,7 @@ async function main() {
   const inputUsername = await page.$('[name="session[username_or_email]"]');
   
   await inputUsername.type(username);
+  
   const inputPassword = await page.$('[name="session[password]"]');
   await inputPassword.type(password);
   await inputPassword.press("Enter");
